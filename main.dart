a52193c3e58de3b5a410c39ac661b15697a50c52
XISEL <roma.kornienko.2005@gmail.com>
2023-11-08 12:34:45 +0300

    branch2 commit

---
+++ main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Container(
+      color: Colors.blueAccent,
+      alignment: Alignment.center,
+      child: Text('Hello Flutter', textDirection: TextDirection.ltr)));
+}
